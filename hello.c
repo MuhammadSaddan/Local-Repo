3d5ee07747d0dec0b29744d80813936b8eb52642
MuhammadSaddan <muhammadsadaan789@gmail.com>
2024-02-19 00:30:43 +0500

    Saving changes to hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+<<<<<<< HEAD
 
 int main() {
     printf("Hello World");
@@ -11,3 +12,10 @@ int main() {
     return 0;
 }
 
+=======
+int main()
+{
+printf(" Hello World ");
+retrun 0;
+}
+>>>>>>> b35bb9caeb438a2392c42a217a3c489d14b5799d
